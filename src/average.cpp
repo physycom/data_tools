8ac0763e2eef591a6b98e163ad0abef3540db087
Stefano Sinigardi <stesinigardi@hotmail.com>
2016-05-05 10:57:31 +0200

    v1.3: enhance data significance and produce plot similar to power spectra

--- src/average.cpp
+++ src/average.cpp
@@ -27,12 +27,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <vector>
 #include <boost/algorithm/string.hpp>
 
-//#include "jsoncons/json.hpp"
 
 #define MAJOR_VERSION 1
-#define MINOR_VERSION 2
+#define MINOR_VERSION 3
+#define SEPARATORS       " \t"
+#define COMMENTS         "#"
 
-void prepare_gnuplot_script_1D(std::ofstream &output_file, std::string data_file, std::string plot_file, size_t Xres, size_t Yres, size_t fontsize, size_t x, size_t y, size_t z, size_t t, size_t q, std::string t_key, std::string q_key, std::string x_key, std::string y_key, std::string title_key) {
+
+void prepare_gnuplot_script_1D_twoplots(std::ofstream &output_file, std::string data_file, std::string plot_file, size_t Xres, size_t Yres, size_t fontsize, size_t x, size_t y, size_t z, size_t t, size_t q, std::string t_key, std::string q_key, std::string x_key, std::string y_key, std::string title_key) {
   output_file << "#!/gnuplot\n";
   output_file << "FILE_IN='" << data_file << "'\n";
   output_file << "FILE_OUT='" << plot_file << "'\n";
@@ -45,6 +47,77 @@ void prepare_gnuplot_script_1D(std::ofstream &output_file, std::string data_file
   output_file << "     FILE_IN u " << t << ":" << q << " w lines lt 1 lc rgb 'red' lw 3 t '" << q_key << "'\n";
   output_file << "\n";
 }
+
+void prepare_gnuplot_script_1D(std::ofstream &output_file, std::string data_file, std::string plot_file, size_t Xres, size_t Yres, size_t fontsize, size_t x, size_t y, std::string x_key, std::string y_key, std::string plot_key, std::string title_key) {
+  output_file << "#!/gnuplot\n";
+  output_file << "FILE_IN='" << data_file << "'\n";
+  output_file << "FILE_OUT='" << plot_file << "'\n";
+  output_file << "set terminal pngcairo size " << Xres << ',' << Yres << " font \"," << fontsize << "\"\n";
+  output_file << "set output FILE_OUT\n";
+  output_file << "set title '" << title_key << "'\n";
+  output_file << "set xlabel '" << x_key << "'\n";
+  output_file << "set ylabel '" << y_key << "'\n";
+  output_file << "plot FILE_IN u " << x << ":" << y << " w lines lt 1 lc rgb 'blue' lw 3 t '" << plot_key << "',\\" << std::endl;
+  output_file << "\n";
+}
+
+bool Belongs_to(char c, std::string s) {
+  for (size_t i = 0; i < s.size(); i++) { if (c == s.at(i)) return true; }
+  return false;
+}
+
+
+std::vector< std::vector<double> > Convert_to_double_vector(std::vector< std::vector<std::string> > parsed_file) {
+  std::vector<double> doubled_line;
+  std::vector< std::vector<double> > doubled_file;
+
+  for (auto &i : parsed_file) {
+    doubled_line.clear();
+    doubled_line.resize(i.size());
+    for (size_t j = 0; j < i.size(); j++) doubled_line[j] = atof(i[j].c_str());
+    doubled_file.push_back(doubled_line);
+  }
+  return doubled_file;
+}
+
+
+std::vector< std::vector<std::string> > Parse_file(std::string file_name, std::string separators, std::string comment) {
+  // Safe file opening
+  std::ifstream file_to_parse(file_name, std::ios::in);
+  if (!file_to_parse) {
+    std::cout << "Cannot open " << file_name << ". Quitting..." << std::endl;
+    exit(12);
+  }
+  // Internal variables
+  std::string line;
+  std::vector<std::string> tokens;
+  std::vector< std::vector<std::string> > parsed;
+  while (getline(file_to_parse, line)) {
+    boost::algorithm::trim(line);  // remove leading/trailing spaces
+    if (Belongs_to(line[0], comment) || !line.size()) continue;
+    boost::algorithm::split(tokens, line, boost::algorithm::is_any_of(separators), boost::token_compress_on);
+    //std::transform(tokens[0].begin(), tokens[0].end(), tokens[0].begin(), ::tolower);
+    for (size_t i = 0; i < tokens.size(); i++) {  // remove inline comments
+      if (Belongs_to(tokens[i][0], comment)) { tokens.erase(tokens.begin() + i, tokens.end()); }
+    }
+    if (tokens.size()) {
+      parsed.push_back(tokens);
+    }
+    line.clear(); tokens.clear();
+  }
+  file_to_parse.close();
+  return parsed;
+}
+
+double Calculate_average(std::vector< std::vector<double> > doubled_file, int col_number) {
+  double average = 0;
+  for (auto &i : doubled_file) {
+    average += i[col_number];
+  }
+  average /= (double)(doubled_file.size());
+  return average;
+}
+
 
 void usage(char * progname) {
   std::cout << "Usage: " << progname << " -period <time_interval_to_average[s]> file1 file2 ... fileN" << std::endl;
@@ -76,50 +149,51 @@ int main(int argc, char ** argv) {
     usage(argv[0]);
   }
 
-  for (auto file : input_files) {
-    std::ifstream file_in(file);
-    if (!file_in) {
-      std::cout << "ERROR: unable to open " << file << std::endl;
-      exit(-2);
-    }
-    else std::cout << std::endl << "Averaging \"" << file << "\" with period " << period << " s" << std::endl;
+  std::cout << std::endl << "Averaging your files with period " << period << " s" << std::endl;
 
-    std::string line;
-    std::vector<std::string> tokens;
-    std::vector<double> values;
+  for (auto file : input_files) {
+    std::vector< std::vector<std::string> > parsed_file = Parse_file(file, SEPARATORS, COMMENTS);
+    std::vector< std::vector<double> > doubled_file = Convert_to_double_vector(parsed_file);
+    double average_ax = Calculate_average(doubled_file, 2);
+    double average_ay = Calculate_average(doubled_file, 3);
+    double average_az = Calculate_average(doubled_file, 4);
+    double forzante = std::stod(file.substr(file.size() - 17, 3));
+
+    std::vector<double> averaged_value;
     std::vector< std::vector<double> > averaged_values;
-    int col_num = 0, line_cnt = 0, period_cnt = 0;
+    size_t col_num = 0, line_cnt = 0, period_cnt = 0;
     double start_time = -1.0;
     double first_time = -1.0;
-    while (std::getline(file_in, line)) {
-      boost::split(tokens, line, boost::is_any_of(" \t"), boost::token_compress_on);
-      if (tokens[0][0] == '#') continue;
+
+    for (auto line : doubled_file) {
       line_cnt++;
       period_cnt++;
 
-      if (start_time < 0.0) start_time = first_time = std::stod(tokens[0]);
+      if (start_time < 0.0) start_time = first_time = line[0];
 
       if (col_num == 0) {
-        col_num = (int)tokens.size()+1; // the last column contains again the timestamp, but starting from zero for the first row
-        values.resize(col_num);
-        for (int i = 0; i < col_num; i++) {
-          values[i] = 0;
+        col_num = line.size() + 2; // the last column contains again the timestamp, but starting from zero for the first row
+        averaged_value.resize(col_num);
+        for (size_t i = 0; i < col_num; i++) {
+          averaged_value[i] = 0.0;
         }
       }
 
-      for (int i = 0; i < col_num-1; i++) {
-        values[i] += std::stod(tokens[i]);
+      for (size_t i = 0; i < col_num - 2; i++) {
+        averaged_value[i] += line[i];
       }
+      averaged_value[col_num - 2] += (line[2] - average_ax) * (line[2] - average_ax) + (line[3] - average_ay) * (line[3] - average_ay) + (line[4] - average_az) * (line[4] - average_az);
 
-      if (std::stod(tokens[0]) - start_time > period) {
-        start_time = std::stod(tokens[0]);
-        for (int i = 0; i < col_num-1; i++) {
-          values[i] /= period_cnt;
+      if (line[0] - start_time > period) {
+        start_time = line[0];
+        for (size_t i = 0; i < col_num - 1; i++) {
+          averaged_value[i] /= period_cnt;
         }
-        values[col_num - 1] = values[0] - first_time;
-        averaged_values.push_back(values);
-        for (int i = 0; i < col_num; i++) {
-          values[i] = 0;
+        averaged_value[col_num - 2] *= (2.0/forzante);
+        averaged_value[col_num - 1] = averaged_value[0] - first_time;
+        averaged_values.push_back(averaged_value);
+        for (size_t i = 0; i < col_num; i++) {
+          averaged_value[i] = 0.0;
         }
         period_cnt = 0;
       }
@@ -137,7 +211,7 @@ int main(int argc, char ** argv) {
     file_out.close();
 
     file_out.open(file.substr(0, file.size() - 4) + "_ave.plt");
-    prepare_gnuplot_script_1D(file_out, file.substr(0, file.size() - 4) + "_ave.txt", file.substr(0, file.size() - 4) + "_ave.png", 1280, 720, 20, 3, 4, 5, col_num, col_num-1, "modulus of average", "average of modulus", "t (s)", "a (g)", file.substr(0, file.size() - 13));
+    prepare_gnuplot_script_1D(file_out, file.substr(0, file.size() - 4) + "_ave.txt", file.substr(0, file.size() - 4) + "_ave.png", 1280, 720, 20, col_num, col_num - 1, "average of modulus", "t (s)", "2*osc^2/g_0 (g)", file.substr(0, file.size() - 13));
   }
 
   return 0;
